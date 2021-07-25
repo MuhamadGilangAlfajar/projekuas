eaafc5117b0c161cc1c3123af6936e76c17b8962
MuhamadGilangAlfajar <muhamad2000018221@webmail.uad.ac.id>
2021-07-25 22:01:08 +0700

    firs file

---
+++ dropdown.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>dropdown</title>
+	<link rel="stylesheet" type="text/css" href="dropdown.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+</head>
+<body>
+	<div class="dropdown">
+		<nav>
+			<label class="logo">Welcome</label>
+			<ul>
+				<li><a href="#">menu <i class="fas fa-caret-down"></i></a>
+					<ul>
+						<li><a href="navigationbar.php">navigation bar</a></li>
+						<li><a href="dropdown.php">dropdown</a></li>
+						<li><a href="#">input data</a></li>
+						<li><a href="#">logout</a></li>
+					</ul>
+				</li>
+			</ul>
+		</nav>
+	</div>
+	<section>
+		<img src="kopi.jpg">
+	</section>
+</body>
+</html>
---
+++ navigationbar.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>responsive nevbar</title>
+	<meta name="viwport"  content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="navigationbar.css">
+	<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+</head>
+<body>
+	<nav>
+		<input type="checkbox" id="check">
+		<label for="check" class="checkbtn">
+			<i class="fas fa-bars"></i>
+		</label>
+		<label class="logo">Welcome</label>
+		<ul>
+			<li><a class="active" href="navigationbar.php">navigation bar</a></li>
+			<li><a href="dropdown.php">dropdown</a></li>
+			<li><a href="#">input data</a></li>
+			<li><a href="#">logout</a></li>
+		</ul>
+	</nav>
+	<section>
+		<img src="kopi.jpg">
+	</section>
+</body>
+</html>
